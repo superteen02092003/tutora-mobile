ddfda01ade501436121aa1801ecc3a6836c9d775
haiquanng <nguyenhaiquan.data@gmail.com>
2026-08-14 22:07:34 +0700

    feat(apps): vi for Locale

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:tutora/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  await initializeDateFormatting('vi_VN');
+  Intl.defaultLocale = 'vi_VN';
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
